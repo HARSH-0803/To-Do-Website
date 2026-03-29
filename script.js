0d631b45b5479c2bf3e396151331868c0f48c468
Harsh Singh <harsh0803singh@gmail.com>
2026-03-29 21:25:27 +0530

    Changes in the Edit Button

--- script.js
+++ script.js
@@ -9,13 +9,29 @@ addBtn.addEventListener("click", () => {
     app.classList.add("blur");
 });
 
+function formatTime(time) {
+    let [hours, minute] = time.split(":");
+    hours = parseInt(hours);
+
+    let ampm = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    return `${hours}:${minute} ${ampm}`;
+}
+
+
+
 saveBtn.addEventListener("click", () => {
 
     const name = document.getElementById("taskNameInput").value;
-    const time = document.getElementById("taskTime").value;
     const priority = document.getElementById("prioritySelect").value;
 
     const date = new Date().toDateString();
+
+    const rawTime = document.getElementById("taskTime").value;
+    const time = formatTime(rawTime);
     const fullDate = time + " | " + date;
 
     const task = document.createElement("div");
@@ -28,11 +44,28 @@ saveBtn.addEventListener("click", () => {
     <button class ="edit">🖊</button>
     <span class = "priority ${priority}"></span>
     `;
+
+    const editBtn = task.querySelector(".edit");
+    const taskText = task.querySelector(".taskName");
+
+    editBtn.addEventListener("click", () => {
+        const newTask = prompt("Edit your Task : ", taskText.innerText);
+
+        if (newTask !== null && newTask.trim() !== "") {
+            taskText.innerText = newTask;
+        }
+        if (name.trim() === "") {
+            alert("Task name required!");
+            return;
+        }
+    });
     app.classList.remove("blur");
 
     list.appendChild(task);
 
     modal.classList.add("hidden");
 });
+
+
 
 
